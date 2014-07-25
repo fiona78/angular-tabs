cf5ab531ea679d7cf1dd9e1838ccae0fd58bba54
Fiona Przybylski <fiona@clearbooks.co.uk>
2014-07-25 17:00:43 +0100

    tab functionality

--- js/cbVTab.js
+++ js/cbVTab.js
@@ -10,27 +10,31 @@ angular.module( 'cbVTabs', [] )
 
         controller: function($scope) {
             var tabs = $scope.tabs = [];
-/*
+
             $scope.select = function(tab) {
                 angular.forEach(tabs, function(tab) {
                     tab.selected = false;
                 });
                 tab.selected = true;
             };
-*/
+
             this.addTab = function(tab) {
-/*
+
                 if (tabs.length === 0) {
                     $scope.select(tab);
                 }
-*/                
+                
                 tabs.push(tab);
-                console.log(tab);
+                //console.log(tab);
             };
 
         },
 
-        template: '<div class="red"></div><div class="blue" ng-transclude></div>',
+        template:
+            '<tab-menu>' + 
+            '<cb-tab-link ng-repeat = "tab in tabs"><a href = "#" ng-click = "select(tab)" ng-focus="select(tab)">{{tab.title}}</a></cb-tab-link>' +
+            '</tab-menu>' + 
+            '<cb-tab-placeholder ng-transclude></cb-tab-placeholder>',
     
         link: function postLink( scope, element, attributes, controller ) { 
 
@@ -42,19 +46,33 @@ angular.module( 'cbVTabs', [] )
     return {
       require: '^cbVTabs',
       restrict: 'E',
-/*
+      transclude: true,
+
       scope: {
         title: '@'
       },
-*/
+
       link: function(scope, element, attrs, tabsCtrl) {
         tabsCtrl.addTab(scope);
-      }
+
+                scope.$apply( function() {
+                
+                    $('cb-tab-link a').on('focus', function(){
+                    //$(this).trigger('click');
+                    console.log("focused");
+                    });
+
+                });
+      },
+      
+       templateUrl: 'panel-content.html'
+    
      
     };
   });
 
 //can continue to add directives.
+
 
 
 
